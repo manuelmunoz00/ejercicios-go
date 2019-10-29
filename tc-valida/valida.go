c6a2db77282a6f77038eb37b09c13ccf6743320c
Manuel Muñoz <manuel.munoz00@gmail.com>
2019-10-28 22:09:12 -0300

    se agregan comentarios para documentación

--- tc-valida/valida.go
+++ tc-valida/valida.go
@@ -21,35 +21,43 @@ func main() {
 	fmt.Println(validar(*nuevaTarjeta))
 }
 
-func validar(tc Tarjeta) string {
+func validar(tc Tarjeta) bool {
+	// se reemplazan guiones por vacios
 	str := strings.Replace(tc.numero, "-", "", -1)
+	// se invierte string de números dado que la validación es de izquierda a derecha
 	strReversa := reversa(str)
 	var eslice []int64
 	for i, r := range strReversa {
 		par := i + 1
 		c := string(r)
+		// se convierte a entero el string de números
 		j, err := strconv.ParseInt(c, 10, 32)
 		if err == nil {
+			// por cada 2 dígitos se duplica el valor
 			if par%2 == 0 {
 				doble := j * 2
+				// si el doble es mayor a 9 se resta al doble el valor de 9
 				if doble > 9 {
 					doble = doble - 9
 				}
+				// se agrega al slice el doble
 				eslice = append(eslice, doble)
 			} else {
+				// se agrega el valor de los índices que no sean pares
 				eslice = append(eslice, j)
 			}
 		}
 	}
+	// se recorre el slice para hacer la sumatoria según la evaluación de los dígitos realizado anteriormente
 	var sumatoria int64
 	for x := 0; x < len(eslice); x++ {
 		sumatoria = sumatoria + eslice[x]
 	}
-	fmt.Println("sumatoria--", sumatoria)
+	// si la sumatoria es divisible por 10 entonces el número es válido
 	if sumatoria%10 == 0 {
-		return "tc valida"
+		return true
 	}
-	return "tc invalida"
+	return false
 }
 
 func reversa(str string) string {
